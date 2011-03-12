6cf7add45bdfe132c237346e1fdbd82eacd841cc
Miles Gould <miles@assyrian.org.uk>
2011-03-12 14:07:43 +0000

    WIP

--- cams.pl
+++ cams.pl
@@ -5,11 +5,12 @@ package Cams;
 use warnings;
 use strict;
 use 5.010;
-use Algorithm::Simplex;
-use Data::Dumper;
+use Set::Object qw/set/;
+use List::Util qw/sum/;
+use POSIX qw/floor/;
 
 my $min = 15;
-my $max = 49;
+my $max = 20;
 
 # Made up data
 my @cams = (
@@ -28,13 +29,58 @@ my @cams = (
     { name => "BC Camalot 6", min => 54, max => 70, weight => 34 },
 );
 
-# Build a Tucker tableau
-my @matrix = ();
-foreach my $width ($min .. $max) { # off-by-one?
-    push @matrix,
-        [ map { $_->{min} <= $width && $_->{max} > $width ? 1 : 0 } @cams];
+sub weight_of_set {
+    my $covering = shift;
+    return sum(map { $_->{'weight'} } $covering->members());
 }
 
-print Dumper @matrix;
+sub covering {
+    my $width = shift;
+    my @suitable = grep { $_->{min} <= $width && $_->{max} >= $width } @cams;
+    return map { set($_) } @suitable;
+}
+
+# Given a list of coverings for L and a list of coverings for R, return a list
+# of coverings for L+R, sorted by cost (lowest first).
+sub merge {
+    say @_;
+    (my $left, my $right, my $cost_fn) = @_;
+    my @answer = ();
+    for my $left_set (@$left) {
+        for my $right_set (@$right) {
+            push @answer, $left_set + $right_set;
+        }
+    }
+    return [ sort { $cost_fn->($a) <=> $cost_fn->($b) } @answer ];
+}
+
+sub candidates {
+    (my $min, my $max, my $cost_fn) = @_;
+    if (($max - $min) == 0) {
+        return [];
+    } elsif (($max - $min ) == 1) {
+        return [covering($min)];
+    } else {
+        my $middle = floor(($max + $min)/2);
+        my $left = candidates($min, $middle, $cost_fn);
+        my $right = candidates($middle, $max, $cost_fn);
+        print("$min-$max: ");
+        my @candidates = merge($left, $right, $cost_fn);
+#        say scalar(@candidates) . " candidates.";
+        return @candidates;
+    }
+}
+
+sub say_covering {
+    my $covering = shift;
+    my $cost_fn = shift;
+    print join(", ", map { $_->{'name'} } $covering->members());
+    print ": " . $cost_fn->($covering) if defined($cost_fn);
+    say;
+}
+
+for my $covering (candidates($min, $max, \&weight_of_set)) {
+    say_covering($covering, \&weight_of_set);
+}
 
 1;
