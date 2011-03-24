abbf9031eef1dcdcca240ba192e4f0c96520e833
Miles Gould <miles@assyrian.org.uk>
2011-03-24 23:17:54 +0000

    Added minimalist Dancer interface to cams solver.

--- lib/cams.pm
+++ lib/cams.pm
@@ -1,7 +1,12 @@
 package cams;
 use Dancer ':syntax';
+use Cams;
 
 our $VERSION = '0.1';
+
+get '/:min/:max' => sub {
+    return Cams::solve(params->{min}, params->{max});
+};
 
 get '/' => sub {
     template 'index';
