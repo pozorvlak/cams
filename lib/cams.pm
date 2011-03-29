ebcbc2b11ee9069c25eacb9d28689ce90b65809d
Miles Gould <miles@assyrian.org.uk>
2011-03-29 16:21:26 +0100

    Slightly less minimalist Dancer output.

--- lib/cams.pm
+++ lib/cams.pm
@@ -1,11 +1,19 @@
 package cams;
 use Dancer ':syntax';
+use Cams::Data 'test_data';
 use Cams;
 
 our $VERSION = '0.1';
 
 get '/:min/:max' => sub {
-    return Cams::solve(params->{min}, params->{max});
+    (my $min, my $max) = (params->{min}, params->{max});
+    my $data = test_data();
+    my $solver = Cams::Solver->new($data, $min, $max, 'mass');
+    template 'results.tt', {
+         min => $min,
+         max => $max,
+         camsets => [ $solver->solutions ]
+    };
 };
 
 get '/' => sub {
