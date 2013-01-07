24619dea99be4688f4f0292953f8140992fcfcfe
Miles Gould <miles@assyrian.org.uk>
2013-01-07 23:35:58 +0000

    Added same licence terms as Perl.

--- lib/Cams.pm
+++ lib/Cams.pm
@@ -22,4 +22,21 @@ sub solve {
     }
 }
 
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2011-2013 Miles Gould <miles@assyrian.org.uk>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of either:
+
+=over 4
+
+=item * the GNU General Public License as published by the Free
+Software Foundation; either version 1, or (at your option) any
+later version, or
+
+=item * the Artistic License version 2.0.
+
+=cut
+
 1;
